c05daef3da0c0fee25d85a843d01794a5b1065fe
Jeremy Alexander <jwalexander22@gmail.com>
2019-02-28 22:28:55 +0000

    added prism.css / prism.js for syntax highlighting

--- functions.php
+++ functions.php
@@ -12,9 +12,9 @@
 //Loading in Scripts
 function make2dgames_enqueue_styles()
 {
-
-    // wp_enqueue_style("add_normalize",  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css', false);
+    wp_enqueue_script('add_prism_js', get_template_directory_uri() . '/assets/js/prism.js');
     wp_enqueue_style("add_google_font",  'https://fonts.googleapis.com/css?family=Nunito', false);
+    wp_enqueue_style("add_prism_css",  get_template_directory_uri() . "/assets/css/prism.css");
     wp_enqueue_style("make2d-custom",  get_template_directory_uri() . "/assets/css/make2d.css");
     wp_enqueue_style("make2dgames-style", get_stylesheet_uri());
     
